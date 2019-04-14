4b84443b83ed6cfe899edea118516a17f94ffd1b
Fredrik Borg <fredrikb@mnemonic.no>
2019-04-14 23:01:41 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# act-types
-Populate the types of the platform
+# ACT types
+
+## Introduction
+
+These scripts are used to add types to the [ACT](https://github.com/mnemonic-no/act-platform) data model (object types and fact types).
+
+## Installation
+1. This project requires that you have a running installation of the [act-platform](https://github.com/mnemonic-no/act-platform).
+2. Install from pip
+```bash
+pip install act-types
+```
+
+## act-types usage
+To bootstrap the type system with default types (userid/act-baseurl must point to ACT installation):
+```
+act-types \
+    --userid 1 \
+    --act-baseurl http://localhost:8888 \
+    --loglevel ERROR \
+    --add-default-object-types \
+    --add-default-fact-types \
+    --add-default-meta-fact-types
+```
+
+To print default types (replace with fact/meta-fact for other types):
+```bash
+act-types --print-default-object-types
+```
+
+## act-graph-datamodel usage
+
+Build a graph (graphviz) of the ACT data model.
+```bash
+act-graph-datamodel --help
+usage: act-graph-datamodel [-h] [--uid UID] [--http_username HTTP_USERNAME]
+                           [--http_password HTTP_PASSWORD]
+                           [--parent_id PARENT_ID]
+                           [--confluence_url CONFLUENCE_URL]
+                           [--confluence_user CONFLUENCE_USER]
+                           [--confluence_password CONFLUENCE_PASSWORD]
+                           url
+```
---
+++ act/types/types.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+
+""" Add ACT types """
+
+import argparse
+import json
+import sys
+from logging import critical, warning
+from typing import Text, Dict
+
+from pkg_resources import resource_string
+
+import act.api
+
+
+def parseargs() -> argparse.Namespace:
+    """ Parse arguments """
+    parser = argparse.ArgumentParser(description="ACT Bootstrap data model")
+    parser.add_argument(
+        "--add-default-object-types",
+        action="store_true",
+        help="Add default object types")
+    parser.add_argument(
+        "--add-default-fact-types",
+        action="store_true",
+        help="Add default fact types")
+    parser.add_argument(
+        "--add-default-meta-fact-types",
+        action="store_true",
+        help="Add default meta fact types")
+    parser.add_argument(
+        "--print-default-object-types",
+        action="store_true",
+        help="Print default object types and exit")
+    parser.add_argument(
+        "--print-default-fact-types",
+        action="store_true",
+        help="Print default fact types and exit")
+    parser.add_argument(
+        "--print-default-meta-fact-types",
+        action="store_true",
+        help="Print default meta fact types and exit")
+    parser.add_argument(
+        "--object-types-file",
+        type=argparse.FileType('r', encoding='UTF-8'),
+        help="Object type defintions (json)")
+    parser.add_argument(
+        "--fact-types-file",
+        type=argparse.FileType('r', encoding='UTF-8'),
+        help="Fact type defintions (json)")
+    parser.add_argument(
+        "--meta-fact-types-file",
+        type=argparse.FileType('r', encoding='UTF-8'),
+        help="Meta Fact type defintions (json)")
+    parser.add_argument("--logfile", help="Log to file (default = stdout)")
+    parser.add_argument("--loglevel", default="info", help="Loglevel (default = info)")
+    parser.add_argument("--userid", type=int, help="User ID")
+    parser.add_argument(
+        "--act-baseurl",
+        dest="act_baseurl",
+        help="API URI")
+
+    return parser.parse_args()
+
+
+def etc_file(filename: Text) -> Text:
+    "Get content of file from resource/etc"
+    return resource_string("act.types", "etc/{}".format(filename)).decode('utf-8')
+
+
+def create_object_types(client: act.api.Act, object_types_json: Text) -> None:
+    """
+    Create object types
+    """
+    try:
+        object_types = json.loads(object_types_json)
+    except json.decoder.JSONDecodeError:
+        critical("Unable to parse file as json: %s" % object_types_json)
+        sys.exit(1)
+
+    existing_object_types = [object_type.name
+                             for object_type in client.get_object_types()]
+
+    # Create all objects
+    for object_type in object_types:
+        name = object_type["name"]
+        validator = object_type.get("validator", act.api.DEFAULT_VALIDATOR)
+
+        if name in existing_object_types:
+            warning("Object type %s already exists" % name)
+            continue
+
+        client.object_type(name=name, validator_parameter=validator).add()
+
+
+def create_fact_types(client, fact_types_json):
+    """
+    Create fact type with allowed bindings to ALL objects
+    We want to change this later, but keep it like this to make it simpler
+    when evaluating the data model
+    """
+
+    try:
+        fact_types = json.loads(fact_types_json)
+    except json.decoder.JSONDecodeError:
+        critical("Unable to parse file as json: %s" % fact_types_json)
+        sys.exit(1)
+
+    for fact_type in fact_types:
+        name = fact_type["name"]
+        validator = fact_type.get("validator", act.api.DEFAULT_VALIDATOR)
+        object_bindings = fact_type.get("objectBindings", [])
+
+        if not object_bindings:
+            client.create_fact_type_all_bindings(
+                name, validator_parameter=validator)
+
+        else:
+            client.create_fact_type(name, validator=validator, object_bindings=object_bindings)
+
+
+def create_meta_fact_types(client, meta_fact_types_json):
+    """
+    Create fact type with allowed bindings to ALL objects
+    We want to change this later, but keep it like this to make it simpler
+    when evaluating the data model
+    """
+
+    try:
+        meta_fact_types = json.loads(meta_fact_types_json)
+    except json.decoder.JSONDecodeError:
+        critical("Unable to parse as json: %s" % meta_fact_types_json)
+        sys.exit(1)
+
+    for meta_fact_type in meta_fact_types:
+        name = meta_fact_type["name"]
+        validator = meta_fact_type.get("validator", act.api.DEFAULT_VALIDATOR)
+        fact_bindings = meta_fact_type.get("factBindings", [])
+
+        if not fact_bindings:
+            client.create_meta_fact_type_all_bindings(name, validator_parameter=validator)
+
+        else:
+            client.create_meta_fact_type(name, fact_bindings=fact_bindings, validator=validator)
+
+
+def main() -> None:
+    "Main function"
+    args = parseargs()
+
+    if args.print_default_object_types:
+        print(etc_file("object-types.json"))
+        sys.exit(0)
+
+    if args.print_default_fact_types:
+        print(etc_file("fact-types.json"))
+        sys.exit(0)
+
+    if args.print_default_meta_fact_types:
+        print(etc_file("meta-fact-types.json"))
+        sys.exit(0)
+
+    client = act.api.Act(
+        args.act_baseurl,
+        args.userid,
+        args.loglevel,
+        args.logfile,
+        "act-types")
+
+    if args.add_default_object_types:
+        create_object_types(client, etc_file("object-types.json"))
+
+    if args.add_default_fact_types:
+        create_fact_types(client, etc_file("fact-types.json"))
+
+    if args.add_default_meta_fact_types:
+        create_meta_fact_types(client, etc_file("meta-fact-types.json"))
+
+    if args.object_types_file:
+        create_object_types(client, args.object_types_file.read())
+
+    if args.fact_types_file:
+        create_fact_types(client, args.fact_types_file.read())
+
+    if args.meta_fact_types_file:
+        create_meta_fact_types(client, args.meta_fact_types_file.read())
+
+
+if __name__ == "__main__":
+    main()
