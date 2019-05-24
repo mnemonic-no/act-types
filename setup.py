adc8eb389cfae81025b42bf6e9eeb370faca4a13
Fredrik Borg <fredrik.borg@gmail.com>
2019-05-24 09:48:56 +0200

    Model events (#4)
    * model events
    
    * attributedTo changes
    
    * remove attributedTo directly to threatactor, remove email, add category to hash, report and tool
    
    * bump version to 0.7.0
    
    * add report mentions ipv4Network

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), "rb") as f:
 
 setup(
     name="act-types",
-    version="0.6.0",
+    version="0.7.0",
     author="mnemonic AS",
     zip_safe=True,
     author_email="opensource@mnemonic.no",
