d8d75ce55f54484d31bfe9df27393cc041e84b77
Fredrik Borg <fredrikb@mnemonic.no>
2021-10-29 14:12:51 +0200

    black formatting

--- act/types/format.py
+++ act/types/format.py
@@ -9,7 +9,7 @@ class ValidationError(Exception):
 
 
 def default_lowercase_format(value: Text) -> Text:
-    """ Default lowercase format
+    """Default lowercase format
     * remove text in paranthesis
     * replace multiple whitespace with single space
     * lowercase
@@ -19,7 +19,7 @@ def default_lowercase_format(value: Text) -> Text:
 
 
 def validate(object_type: Text, object_value: Text):
-    """ Validate object type/value """
+    """Validate object type/value"""
     if not object_validates(object_type, object_value):
         raise ValidationError(f"{object_type} does not validate: {object_value}")
 
@@ -27,12 +27,12 @@ def validate(object_type: Text, object_value: Text):
 
 
 def format_tool(tool: Text) -> Text:
-    """ Format and validate tool name """
+    """Format and validate tool name"""
 
     return validate("tool", default_lowercase_format(tool))
 
 
 def format_threat_actor(threat_actor: Text) -> Text:
-    """ Format and validate Threat Actor name """
+    """Format and validate Threat Actor name"""
 
     return validate("threatActor", default_lowercase_format(threat_actor))
